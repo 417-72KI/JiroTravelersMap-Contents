73b14ba4b3daad3c05e52d7a7bbb4d5076022bc9
417-72KI <417.72ki@gmail.com>
2020-03-23 01:19:44 +0900

    status

--- containers/crawl/index.js
+++ containers/crawl/index.js
@@ -69,6 +69,7 @@ async function execute(id, url) {
         id: id,
         kind: 'origin',
         name: name,
+        status: 'open',
         prefecture: prefecture[address.prefecture],
         address: address.address,
         location: location,
