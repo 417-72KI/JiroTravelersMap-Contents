e09381c23ec97f59ec5d6adb25b289ff57628232
417-72KI <417.72ki@gmail.com>
2026-09-26 14:55:19 +0900

    remove PathKit

--- containers/validate/Package.swift
+++ containers/validate/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -14,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.8.2"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
         .package(url: "https://github.com/417-72KI/JiroTravelersMap-Model", branch: "main"),
     ],
     targets: [
@@ -22,7 +20,6 @@ let package = Package(
             name: "JiroTravelersMapContentsValidator",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "PathKit",
                 .product(name: "JiroTravelersMapModel", package: "JiroTravelersMap-Model"),
             ]
         ),
