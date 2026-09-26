db69db28830f8d155d4d5ac5cee58ea9cebc0557
417-72KI <417.72ki@gmail.com>
2026-09-26 14:54:10 +0900

    add upcoming feature flags in Swift 6.2

--- containers/validate/Package.swift
+++ containers/validate/Package.swift
@@ -28,3 +28,25 @@ let package = Package(
         ),
     ]
 )
+
+// MARK: -
+extension SwiftSetting {
+    static let existentialAny: Self = .enableUpcomingFeature("ExistentialAny")                                    // SE-0335, Swift 5.6,  SwiftPM 5.8+
+    static let internalImportsByDefault: Self = .enableUpcomingFeature("InternalImportsByDefault")                // SE-0409, Swift 6.0,  SwiftPM 6.0+
+    static let memberImportVisibility: Self = .enableUpcomingFeature("MemberImportVisibility")                    // SE-0444, Swift 6.1,  SwiftPM 6.1+
+    static let inferIsolatedConformances: Self = .enableUpcomingFeature("InferIsolatedConformances")              // SE-0470, Swift 6.2,  SwiftPM 6.2+
+    static let nonisolatedNonsendingByDefault: Self = .enableUpcomingFeature("NonisolatedNonsendingByDefault")    // SE-0461, Swift 6.2,  SwiftPM 6.2+
+    static let immutableWeakCaptures: Self = .enableUpcomingFeature("ImmutableWeakCaptures")                      // SE-0481, Swift 6.2,  SwiftPM 6.2+
+}
+
+package.targets.forEach {
+    $0.swiftSettings = [
+        .existentialAny,
+        .internalImportsByDefault,
+        .memberImportVisibility,
+        .inferIsolatedConformances,
+        .nonisolatedNonsendingByDefault,
+        .immutableWeakCaptures,
+        .defaultIsolation(MainActor.self),
+    ]
+}
